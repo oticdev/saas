eacb236b8739d79aa12e9c29a0db2b69fccc2deb
oticdev <victorojo007@gmail.com>
2026-04-11 23:08:22 +0100

    Update dependencies for Clerk integration and enhance FastAPI backend with dotenv support for environment variables

--- api/index.py
+++ api/index.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI  # type: ignore
 from fastapi.responses import StreamingResponse  # type: ignore
 from openai import OpenAI  # type: ignore
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.environ["OPENROUTER_API_KEY"],
+)
 
 app = FastAPI()
 
 @app.get("/api")
 def idea():
-    client = OpenAI()
+    
     prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
-    stream = client.chat.completions.create(model="gpt-5-nano", messages=prompt, stream=True)
+    stream = client.chat.completions.create(model="openai/gpt-5-nano", messages=prompt, stream=True)
 
     def event_stream():
         for chunk in stream:
